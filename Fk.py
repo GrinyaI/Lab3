a34c764ff0ddb9bc0a91ba8546dc802697c30ed2
GrinyaI <grinevich2018@mail.ru>
2023-05-01 20:29:48 +0600

    Done

--- Fk.py
+++ Fk.py
@@ -1,21 +1,19 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, url_for
 
 app = Flask(__name__)
 
 @app.route('/')
-@app.route('/index')
 def index():
     return render_template("index.html")
 
-@app.route('/', methods=['post', 'get'])
-def form():
+@app.route('/authorization', methods=['post', 'get'])
+def authoriz():
     data = {}
     au = ''
-    with open('Bd.txt') as file:
+    with open('Bd.txt', 'r') as file:
         for line in file:
             key, *value = line.split()
             data[key] = value
-    print(data)
     if request.method == 'POST':
         log = str(request.form.get('login').strip())
         pas = str(request.form.get('password').strip())
@@ -26,4 +24,26 @@ def form():
                 au = 'Пароль введён неверно'
         else:
             au = 'Логин отсутствует/введён неверно'
-    return render_template('index.html', auth=au)
+    return render_template('authorization.html', ath=au)
+
+@app.route('/registration', methods=['post', 'get'])
+def registr():
+    data = {}
+    reg = ''
+    if request.method == 'POST':
+        log = str(request.form.get('login').strip())
+        pas = str(request.form.get('password').strip())
+        with open('Bd.txt', 'r') as file:
+            for line in file:
+                key, *value = line.split()
+                data[key] = value
+            if data.get(log):
+                reg = 'Данный логин занят'
+            else:
+                with open('Bd.txt', 'a') as writ:
+                    try:
+                        writ.write(log + ' ' + pas + '\n')
+                        reg = 'Регистрация выполнена'
+                    except:
+                        reg = 'Регистрация не выполнена'
+    return render_template('registration.html', regis=reg)
